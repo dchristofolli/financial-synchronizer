1ec457e5cad1363b98ac04b4ef65321607dc1e8c
dchristofolli <danielchristofolli@gmail.com>
2020-07-02 10:10:27 -0300

    Correção no valor do saldo no csv de saída

--- src/main/java/br/dchristofolli/projects/financialsynchronizer/model/ContaCorrenteProcessada.java
+++ src/main/java/br/dchristofolli/projects/financialsynchronizer/model/ContaCorrenteProcessada.java
@@ -10,7 +10,7 @@ public class ContaCorrenteProcessada {
     public ContaCorrenteProcessada(String agencia, String conta, double saldo, String status, boolean update) {
         this.agencia = agencia;
         this.conta = conta;
-        this.saldo = saldo;
+        this.saldo = saldo/100;
         this.status = status;
         this.update = update;
     }
